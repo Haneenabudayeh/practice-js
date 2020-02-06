7b983949b622325b05a1b34510574603c13af79a
haneenabudayeh <haneenabudayeh5@gmail.com>
2020-02-06 09:32:10 +0200

    lab07

---
+++ lab08.js
@@ -0,0 +1,13 @@
+var h =prompt ("enter name");
+var i = 0;
+do {
+  i += 1;
+  console.log(i);
+} while (i < 5);
+document.write ("haneen abudayeh");
+
+
+            for(count = 0; count < 10; count++) {
+               document.write("Current Count : " + count );
+               document.write("<br />");
+            }  
